5713b54bed2c90612d9bebd358cbff66edf2e56a
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-29 19:21:47 +0100

    Added logic when users submits the date, it gets the time difference

--- sandbox.js
+++ sandbox.js
@@ -17,4 +17,61 @@ window.onload = function(){
   currentYear.value = currentYearValue;
   currentYear.setAttribute("readonly", "readonly");
 
+  var reset  document.querySelector("input[type='reset']");
+
+  var date = document.getElementById("date");
+  var month = document.getElementById("month");
+  var year = document.getElementById("year");
+
+  form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    var dateValue = date.value;
+    var monthValue = month.value - 1;
+    var yearValue = year.value;
+
+    var setDate = new Date();
+    setDate.setDate(dateValue);
+    setDate.setMonth(monthValue);
+    setDate.setFullYear(yearValue);
+
+    var currDate = new Date();
+
+    if(dateValue === "" || monthValue === "" || yearValue === ""){
+      alert("Please enter valid value");
+      time.innerHTML = "";
+    }
+
+    if(setDate.getTime() > currDate.getTime()){
+      var diff = setDate - currDate;
+      const mins = Math.round(diff / 1000 / 60);
+      const hours = Math.round(mins / 60);
+      const days = Math.round(hours / 24);
+      const weeks = Math.round(days / 7);
+
+      var time = document.createElement("p");
+      time.classList.add("hi");
+      time.innerHTML = "<p>Time left: <br /> " + weeks + " weeks, " + days + " days, " + hours + " hours</p>";
+      form.appendChild(time);
+    } else{
+      var diff = currDate - setDate;
+      const mins = Math.round(diff / 1000 / 60);
+      const hours = Math.round(mins / 60);
+      const days = Math.round(hours / 24)
+      const weeks = Math.round(days / 7);
+      const months = Math.round(days / 60);
+
+      var time = document.createElement("p");
+      time.classList.add("hi");
+      time.innerHTML = "<p>Time passed: " + months + " months, " + weeks + " weeks, " + days + " days, " + hours + " hours</p>";
+      form.appendChild(time);
+     }
+
+     reset.addEventListener("click", function(){
+       form.removeChild(time);
+     });
+
+  });
+
 }
